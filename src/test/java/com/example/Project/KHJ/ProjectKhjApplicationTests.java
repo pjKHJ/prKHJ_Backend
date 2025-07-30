b6a981ac6ad0954d0960093501493b4d1fda51c3
규일 <siheun9@gmail.com>
2025-07-30 16:50:35 +0900

    첫 커밋

---
+++ src/test/java/com/example/Project/KHJ/ProjectKhjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project.KHJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKhjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
